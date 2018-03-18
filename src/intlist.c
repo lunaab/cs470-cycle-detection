2392e268b38d7c55989de43fbb0f5d7718ef3b86
lunaab <lunaab@dukes.jmu.edu>
2018-03-18 14:58:12 -0400

    Coded basics for linked list of integers including header file for structs/definitions and intlist.c for implementations of those methods

---
+++ src/intlist.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "intlist.h"
+
+void intlist(struct intlist* l) {
+    l->head = NULL;
+    l->tail = NULL;
+    l->length = 0;
+}
+
+int append(struct intlist* l, int value) {
+    struct node* new;
+    new = (struct node*)malloc(sizeof(struct node));
+    if (new == NULL) {
+        return -1;
+    }
+    
+    new->value = value;
+    new->next = NULL;
+
+    if (l->tail != NULL) {
+        l->tail->next = new;
+    }
+
+    l->tail = new;
+
+    if (l->head == NULL) {
+        l->head = l->tail
+    }
+
+    l->length = l->length + 1;
+
+    return 0;
+}
+
+int isempty(struct intlist* l) {
+    return l->length == 0;
+}
